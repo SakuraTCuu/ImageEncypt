076eb8c659f7ea9c37ca6bab859af9649b7190f5
wangkun <2117810663@qq.com>
2023-08-14 16:23:50 +0800

    优化

--- config.js
+++ config.js
@@ -2,15 +2,12 @@ const path = require("path");
 
 const Config = {
     key: "0123456789abcdef", //密钥
-    iv: "0123456789abcdef",
-    encypt_prefix: "encypt", //加密的固定前缀, 用于识别
-
+    encypt_prefix: "0123456789abcdef", //加密的固定前缀, 用于识别
     suffix: ".png", //加密文件的后缀名
     imgPath: path.join(__dirname, "assets/img"), //源图片路径
     compressPath: path.join(__dirname, "assets/compress"), //压缩后图片路径
     outPath: path.join(__dirname, "assets/out"), //加密后的图片路径
     decPath: path.join(__dirname, "assets/dec"), //解密后的图片路径
-
     debug: true,
 }
 
--- src/encypt_data.js
+++ src/encypt_data.js
@@ -5,8 +5,6 @@ function printInfo() {
     info += "=========================info==============================\n";
     info += "key              " + Config.key + "\n";
     info += "keyBuffer        " + getKeyBuffer() + "\n";
-    info += "iv               " + Config.iv + "\n";
-    info += "prefix           " + Config.encypt_prefix + "\n";
     info += "imgPath          " + Config.imgPath + "\n";
     info += "outPath          " + Config.outPath + "\n";
     info += "=========================info==============================";
@@ -28,26 +26,11 @@ function getKeyBuffer() {
  */
 function encryptBuffer(originBuffer) {
     let keyBuff = getKeyBuffer();
+
     //加点东西 直接拼接
     let buffer = Buffer.concat([keyBuff, originBuffer]);
     buffer = buffer.reverse();
     return buffer;
-
-    // console.log(originBuffer.byteLength);
-
-    // let tempBuffer = Buffer.alloc(0);
-    // let sBuffer = Buffer.alloc(1).fill(1);
-    // for (let i = 0; i < originBuffer.byteLength; i += 128) {
-    //     let end = i + 128;
-    //     if (end >= originBuffer.byteLength) {
-    //         end = originBuffer.byteLength;
-    //     }
-    //     tempBuffer = Buffer.concat([tempBuffer, sBuffer, originBuffer.subarray(i, end)]);
-    // }
-    // console.log(tempBuffer.byteLength);
-    // tempBuffer = tempBuffer.reverse();
-
-    // return tempBuffer;
 }
 
 /**
@@ -62,23 +45,6 @@ function decryptBuffer(encyptBuffer) {
     buffer = buffer.subarray(byteLen);
 
     return buffer;
-
-
-    // console.log(encyptBuffer.byteLength);
-
-    // encyptBuffer = encyptBuffer.reverse();
-
-    // let tempBuffer = Buffer.alloc(0);
-    // for (let i = 0; i < encyptBuffer.byteLength; i += 129) {
-    //     let end = i + 129;
-    //     if (end >= encyptBuffer.byteLength) {
-    //         end = encyptBuffer.byteLength;
-    //     }
-    //     tempBuffer = Buffer.concat([tempBuffer, encyptBuffer.subarray(i + 1, end)]);
-    // }
-    // console.log(tempBuffer.byteLength);
-
-    // return tempBuffer;
 }
 
 module.exports = {
